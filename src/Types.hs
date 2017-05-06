15660316f0cabb5ea8cdc3e59f4b9c2e1f147f55
eliza0x <me@eliza.link>
2017-05-07 01:47:56 +0900

    Update: change Show Term

--- src/Types.hs
+++ src/Types.hs
@@ -9,19 +9,19 @@ data Term =
     TmZero
   | TmTrue
   | TmFalse
-  | TmIsZero Term
-  | TmPred   Term
-  | TmSucc   Term
-  | TmIf   Term Term Term
+  | TmIsZero  Term
+  | TmPred    Term
+  | TmSucc    Term
+  | TmIf      Term  Term  Term
   | NoRuleApplies String
   deriving Eq
 
 instance Show Term where
-    show (TmIsZero x) = "iszero? " ++ show x
-    show (TmSucc x)   = "succ " ++ show x
-    show (TmPred x)   = "pred " ++ show x
-    show (TmIf x y z)   = "if " ++ show x ++ " then " ++ show y ++ " else " ++ show z
-    show TmZero       = "()"
-    show TmTrue       = "true"
-    show TmFalse      = "false"
+    show (TmIsZero x)        = "iszero? " ++ show x
+    show (TmSucc x)          = "succ " ++ show x
+    show (TmPred x)          = "pred " ++ show x
+    show (TmIf x y z)        = "if (" ++ show x ++ ") then (" ++ show y ++ ") else (" ++ show z ++ ")"
+    show TmZero              = "zero"
+    show TmTrue              = "true"
+    show TmFalse             = "false"
     show (NoRuleApplies str) = "[" ++ str ++ "]"
