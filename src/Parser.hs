bceb9e95f2bb318ece992a1222c36112cafeab01
eliza0x <me@eliza.link>
2017-05-07 01:43:11 +0900

    Update: taburalize

--- src/Parser.hs
+++ src/Parser.hs
@@ -6,14 +6,14 @@ import Types
 -- import Control.Monad.State
 
 parser :: Simbol -> Term
-parser (SmAtom "zero")       = TmZero
-parser (SmAtom "true")       = TmTrue
-parser (SmAtom "false")      = TmFalse
+parser (SmAtom "zero")              = TmZero
+parser (SmAtom "true")              = TmTrue
+parser (SmAtom "false")             = TmFalse
 parser (SmList (SmAtom "iszero":t)) = TmIsZero . parser . SmList $ t
 parser (SmList (SmAtom "pred":t))   = TmPred . parser . SmList $ t
 parser (SmList (SmAtom "succ":t))   = TmSucc . parser . SmList $ t
 parser (SmList (SmAtom "if":b:t:f)) = TmIf (parser b) (parser t) (parser . SmList $ f)
-parser (SmList [SmList t])   = parser . SmList $ t
-parser (SmList [SmAtom t])   = parser . SmAtom $ t
-parser _                     = NoRuleApplies "Missing sematic analysis"
+parser (SmList [SmList t])          = parser . SmList $ t
+parser (SmList [SmAtom t])          = parser . SmAtom $ t
+parser _                            = NoRuleApplies "Missing sematic analysis"
 
